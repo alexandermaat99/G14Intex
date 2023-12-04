69702660060bd315b4114e703aeb1d24da3ca53b
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-12-04 14:33:18 -0700

    add/ new dns

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d testing1g14.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email aamaat99@gmail.com
+sudo certbot -n -d provosocial.org --nginx --agree-tos --email aamaat99@gmail.com
