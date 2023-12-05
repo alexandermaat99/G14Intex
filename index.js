b47c26c19bde1dccdd126daa5b984992fda4a5d1
William Koelliker <willkoelliker22@gmail.com>
2023-12-05 09:15:23 -0700

    routes

--- index.js
+++ index.js
@@ -29,10 +29,37 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Route to render the login.ejs file
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login"); // Render the login form
 });
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await knex("users").where({ username }).first();
+
+    if (!user) {
+      // User not found
+      return res.status(401).send("Invalid credentials");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      // Authentication successful
+      // Create a session or JWT token to manage user sessions
+      // Redirect to a protected user page or perform other actions as needed
+      res.redirect("/userPage");
+    } else {
+      // Incorrect password
+      res.status(401).send("Invalid credentials");
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 
 // Route to render the userPage.ejs file
 app.get("/userPage", (req, res) => {
